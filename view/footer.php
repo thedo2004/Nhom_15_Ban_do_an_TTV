918063a5cc0548ec8b09c7773e1a5ccae457243b
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-07-19 19:13:46 +0700

    Create footer.php

---
+++ view/footer.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/home.css">
+
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+    <link href="assets/css/footer.css" rel="stylesheet" />
+ 
+    
+</head>
+<body>
+  <footer class="footer">
+    <div class="container">
+     <div class="row">
+       <div class="footer-col">
+         <h4>Hệ thống cửa hàng H2T</h4>
+         <ul>
+           <li><a href="">H2T STUDIO FLAGSHIP STORE: TRINH VAN BO, HA NOI</a></li>
+           <li><a href="">Store 2: Thon 10 LIEN HIEP, PHUC THO, HA NOI</a></li>
+         </ul>
+       </div>
+       <div class="footer-col">
+         <h4>Chính sách</h4>
+         <ul>
+           <li><a href="">Chính sách sử dụng website</a></li>
+           <li><a href="">Phương thức thanh toán</a></li>
+           <li><a href="">Chính sách đổi trả</a></li>
+           <li><a href="">Chính sách giao nhận - vận chuyển</a></li>
+           <li><a href="">Điều khoản dịch vụ</a></li>
+           <li><a href="">Chính sách bảo mật</a></li>
+         </ul>
+       </div>
+       <div class="footer-col">
+         <h4>Thông tin liên hệ</h4>
+         <ul>
+           <li><a href="">CÔNG TY TNHH H2T</a></li>
+           <li><a href="">Số CSKH: 0325903017 (10h-18h)</a></li>
+           <li><a href="">Ngày cấp: 20/07/2022</a></li>
+           <li><a href="">Tuyển dụng: hr@h2t.vn</a></li>
+           <li><a href="">Website: h2t.com</a></li>
+           <li><a href="">For business: contact@h2t.vn</a></li>
+         </ul>
+       </div>
+       <div class="footer-col">
+         <h4>follow us</h4>
+         <div class="social-links">
+           <a href="#"><i class="fa fa-facebook-square"></i></a>
+           <a href="#"><i class="fa fa-twitter-square"></i></a>
+           <a href="#"><i class="fa fa-instagram"></i></a>
+           <a href="#"><i class="fa fa-link"></i></a>
+         </div>
+       </div>
+     </div>
+    </div>
+    </div>
+ </footer>

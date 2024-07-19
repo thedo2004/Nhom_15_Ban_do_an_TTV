283fbb8f408ea2274f01516bb3493f8be4b39758
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-07-19 18:47:32 +0700

    Create header.php

---
+++ admin/header.php
@@ -0,0 +1,40 @@
+
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="../css/phantrang.css">
+</head>
+<body>
+    <div class="container">
+        <div class="sidebar1">
+   <div class="menu1">
+   <div class="menu-item1">
+           <a href="../index.php"> 
+       <img class="img" src="../img/logo.png" alt="" width="200px"/>
+       </a>
+       </div>
+       <div class="menu-item1">
+           <a href="index.php?act=listdm">DANH MỤC</a>
+       </div>
+       <div class="menu-item1">
+           <a href="index.php?act=listsp">SẢN PHẨM</a>
+       </div>
+       <div class="menu-item1">
+           <a href="index.php?act=listtk">KHÁCH HÀNG</a>
+       </div>
+       <div class="menu-item1">
+           <a href="index.php?act=listkt">KIỂM TRA</a>
+       </div>
+       <div class="menu-item1">
+           <a href="index.php?act=listbill">ĐƠN HÀNG</a>
+       </div>
+       <div class="menu-item1">
+           <a href="index.php?act=listthongke">THỐNG KÊ</a>
+       </div>
+   </div>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</div>

233bb920beaf326700ed9bf9fe7960b3dbc4d6d8
nguyenvanvu2004 <vu19072k4@gmail.com>
2024-07-19 19:13:34 +0700

    Create banner.php

---
+++ view/banner.php
@@ -0,0 +1,7 @@
+<!-- </div>
+  <img src="https://theme.hstatic.net/1000306633/1000891824/14/slideshow_1.jpg?v=587" alt="" style=""/>
+  <div class="banner" >
+    <div class="hinh"> 
+        <img src="https://theme.hstatic.net/1000306633/1000891824/14/block_home_category1_new.png?v=587" alt="" /></div>
+    <div class="hinh"> <img src="https://theme.hstatic.net/1000306633/1000891824/14/block_home_category2_new.png?v=587" alt="" /></div>
+    <div class="hinh"> <img src="https://theme.hstatic.net/1000306633/1000891824/14/block_home_category3_new.png?v=587" alt="" /></div> -->

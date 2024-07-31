d50de1b43bd5622b1c95bf9699a2fc04cc2ecbe6
thedo2004 <dongocthe232004@gmail.com>
2024-07-31 07:15:00 +0700

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,34 +1,4 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/bt1.css">
-</head>
-<body onload="loadImg()">
-    <div class="boxcenter">
-        <div class="row mb header">
-            <h1>Siêu thị trực tuyến</h1>
-        </div>
-        <div class="row mb menu">
-            <ul>
-                <li><a href="index.php?act=home">Trang chủ</a></li>
-                <li><a href="index.php?act=gt">Giới Thiệu</a></li>
-                <li><a href="">Liên hệ</a></li>
-                <li><a href="">Góp ý</a></li>
-                <li><a href="">Hỏi đáp</a></li>
-            </ul>
-        </div>
-        <div class="row mb ">
-            <div class="boxtrai mr ">
-                <div class="row">
-                    <div class="banner" >
-                        <img  alt="" name="img_banner">
-                    </div>
-                </div> -->
-                <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">

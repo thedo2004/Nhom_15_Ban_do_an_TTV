4bbf360f9ffcc5a125bfb5e02d43750ff10efef7
thedo2004 <dongocthe232004@gmail.com>
2024-08-05 19:47:18 +0700

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -18,6 +18,9 @@
        </a>
        </div>
        <div class="menu-item1">
+           <a href="index.php?act=listthongke">THỐNG KÊ</a>
+       </div>
+       <div class="menu-item1">
            <a href="index.php?act=listdm">DANH MỤC</a>
        </div>
        <div class="menu-item1">
@@ -32,9 +35,7 @@
        <div class="menu-item1">
            <a href="index.php?act=listbill">ĐƠN HÀNG</a>
        </div>
-       <div class="menu-item1">
-           <a href="index.php?act=listthongke">THỐNG KÊ</a>
-       </div>
+       
    </div>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </div>
